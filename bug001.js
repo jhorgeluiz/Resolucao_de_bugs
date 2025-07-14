81be0db12dc61c3c6a440bd0858184325aceabdb
Jorge Luiz <159719496+jhorgeluiz@users.noreply.github.com>
2025-07-14 13:55:49 -0300

    Add files via upload

---
+++ bug001.js
@@ -0,0 +1,16 @@
+// function calcularDesconto(preco, tipoCliente) {
+//   if (tipoCliente == "premium") {
+//     return preco * 0.8; // 20% de desconto
+//   } else {
+//     return preco;
+//   }
+// }
+
+// console.log("Cliente comum;",calcularDesconto(100, "comum"));  // Esperado: 100
+// console.log("Cliente premium:",calcularDesconto(100, "premium")); // Esperado: 80
+
+// modifique o sinal de atribuição " = " para sinal de igualdade " == " aonde for premium e melhorei a saida com um texto mais explicativo
+
+
+
+
