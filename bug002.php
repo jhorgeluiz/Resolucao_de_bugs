a0755e7ae1e6a311cdce4364319da850a0eca04e
Jorge Luiz <159719496+jhorgeluiz@users.noreply.github.com>
2025-07-14 14:11:32 -0300

    Update bug002.php

--- bug002.php
+++ bug002.php
@@ -1,4 +1,19 @@
 <?php
+// Erro de verificação de campo vazio
+$usuario = $_POST['usuario'] ?? '';
+
+if ($usuario = '') {
+  echo "Por favor, preencha o nome de usuário.";
+} else {
+  echo "Usuário informado: $usuario";
+}
+
+
+
+
+
+
+// Resolução: 
 $usuario = $_POST['usuario'] ?? '';
 
 if ($usuario === '') {
@@ -6,3 +21,5 @@ if ($usuario === '') {
 } else {
   echo "Usuário informado: $usuario";
 }
+// Na estrutura condiciona if/else, foi identificado que o sinal de atribuição estava errado. 
+// O sinal de atribuição " = " foi corrigido para o sinal de semelhante " === "
