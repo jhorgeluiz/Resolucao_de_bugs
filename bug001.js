a3aeaf1126982f20615187ed2be8067fc4e0ec03
Jorge Luiz <159719496+jhorgeluiz@users.noreply.github.com>
2025-07-14 14:07:56 -0300

    Update bug001.js

--- bug001.js
+++ bug001.js
@@ -1,15 +1,22 @@
-// function calcularDesconto(preco, tipoCliente) {
-//   if (tipoCliente == "premium") {
-//     return preco * 0.8; // 20% de desconto
-//   } else {
-//     return preco;
-//   }
-// }
+❓Problema: O desconto está sempre sendo aplicado, mesmo quando o cliente não é "premium".
 
-// console.log("Cliente comum;",calcularDesconto(100, "comum"));  // Esperado: 100
-// console.log("Cliente premium:",calcularDesconto(100, "premium")); // Esperado: 80
 
-// modifique o sinal de atribuição " = " para sinal de igualdade " == " aonde for premium e melhorei a saida com um texto mais explicativo
+function calcularDesconto(preco, tipoCliente) {
+  if (tipoCliente == "premium") {
+    return preco * 0.8; // 20% de desconto
+  } else {
+    return preco;
+  }
+}
+
+console.log("Cliente comum;",calcularDesconto(100, "comum"));  // Esperado: 100
+console.log("Cliente premium:",calcularDesconto(100, "premium")); // Esperado: 80
+
+
+
+Resolução: 
+Na estrutura condiciona if/else, foi identificado que o sinal de atribuição estava errado. 
+O sinal de atribuição " = " foi corrigido para o sinal de igualdade " == " e melhorei a saida com um texto mais explicativo
 
 
 
